cd614fb0995c24cd6ff0f0edb29cb553324a8394
kauafelixx <161839119+kauafelixx@users.noreply.github.com>
2025-01-25 20:16:53 -0300

    Initial commit

---
+++ src/classes/Player.js
@@ -0,0 +1,25 @@
+class Player {
+    constructor(canvasWidth, canvasHeight) {
+        this.width = 100;
+        this.height = 100;
+        this.velocit=7
+        this.position = {
+            x: canvasWidth/2 -this.width/2,
+            y: canvasHeight - this.height- 20,
+        };
+    }
+
+moveLeft(){
+    this.position.x -=this.velocit
+}
+moveRight(){
+    this.position.x +=this.velocit
+}
+
+    draw(ctx) {
+        ctx.fillStyle = "red"
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+}
+
+export default Player;
---
+++ src/index.js
@@ -0,0 +1,79 @@
+import Player from "./classes/Player.js";
+
+const canvas=document.querySelector('canvas');
+const ctx=canvas.getContext('2d');
+
+canvas.width=innerWidth;
+canvas.height=innerHeight;
+//canvas em 100% da tela
+
+const  player=new Player(canvas.width, canvas.height);
+
+const keys ={
+left: false,
+rigth: false,
+}
+
+const btnLeft = document.querySelector('.btm');
+const btnRight = document.querySelector('.btm2');
+
+const gameLoop= ()  =>{
+ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+if(keys.left && player.position.x >=0){
+    player.moveLeft()
+}
+if(keys.rigth && player.position.x <= canvas.width - player.width){
+player.moveRight()
+}
+
+    player.draw(ctx);
+
+    requestAnimationFrame(gameLoop)
+}
+
+
+
+
+
+addEventListener("keydown", (event) => {
+    const key= event.key.toLowerCase();
+  
+    if(key === "q" || key==="Q")  keys.left= true
+    
+
+    if(key === "e" || key==="E")  keys.rigth=true
+
+
+});
+
+addEventListener("keyup", (event) => {
+    const key= event.key.toLowerCase();
+  
+    if(key === "q" || key==="Q")   keys.left=false;
+
+
+    if(key === "e" || key==="E")    keys.rigth=false;
+    
+});
+
+btnLeft.addEventListener('touchstart', () => {
+    keys.left = true;
+    console.log('Botão esquerdo pressionado', keys);
+  });
+  btnLeft.addEventListener('touchend', () => {
+    keys.left = false;
+    console.log('Botão esquerdo liberado', keys);
+  });
+  btnRight.addEventListener('touchstart', () => {
+    keys.right = true;
+    console.log('Botão direito pressionado', keys);
+  });
+  btnRight.addEventListener('touchend', () => {
+    keys.right = false;
+    console.log('Botão direito liberado', keys);
+  });
+
+
+
+gameLoop();
